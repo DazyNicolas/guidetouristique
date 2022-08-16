22672ba97eddabb332a5f8fc9923a3adf62c0c17
Dazyexelle007 <luciarasambahita1999gmail.com>
2022-08-16 20:12:37 +0300

    add show page and add link betwenn page

--- src/Controller/PublicationsController.php
+++ src/Controller/PublicationsController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PublicationRepository;
+use App\Entity\Publication;
 
 class PublicationsController extends AbstractController
 {
@@ -17,5 +18,11 @@ class PublicationsController extends AbstractController
        // dump($publicationsRepository ->findAll());
        $publications = $publicationsRepository -> findAll();
         return $this->render('publications/index.html.twig', compact('publications'));
+    }
+    #[Route('/publications/{id<[0-9]+>}', name: 'app_publication_show')]
+
+    public function show(Publication $publication): Response
+    {
+        return $this->render('publications/show.html.twig', compact('publication'));
     }
 }
