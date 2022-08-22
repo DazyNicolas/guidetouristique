af5f34edc9b8109c174279081f2a89d6af51bb85
Dazyexelle007 <luciarasambahita1999gmail.com>
2022-08-22 11:36:13 +0300

    add vichUploader to publications image and fix nav

--- src/Entity/Publication.php
+++ src/Entity/Publication.php
@@ -7,11 +7,14 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\Traits\Timestampable;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 
 #[ORM\Entity(repositoryClass: PublicationRepository::class)]
 #[ORM\Table(name:"publications")]
 #[ORM\HasLifecycleCallbacks]
+#[Vich\Uploadable] 
 class Publication
 {
     use Timestampable;
@@ -30,6 +33,10 @@ class Publication
     #[Assert\NotBlank()]
     #[Assert\Length(min: 5)]
     private ?string $description = null;
+
+     // NOTE: This is not a mapped field of entity metadata, just a simple property.
+     #[Vich\UploadableField(mapping: 'publication_image', fileNameProperty: 'imageName')]
+     private ?File $imageFile = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $imageName = null;
@@ -61,6 +68,32 @@ class Publication
         $this->description = $description;
 
         return $this;
+        
+    }
+
+     /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
+     */
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->setUpdatedAt(new \DateTimeImmutable);
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
     }
 
     public function getImageName(): ?string
