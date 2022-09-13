04acc84544a1f0a7bbe14cc35eeb73dc21d1fda8
Dazyexelle007 <luciarasambahita1999gmail.com>
2022-09-13 16:24:06 +0300

    fix edit account and an creadetAt to template

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\GuideFormType;
+use App\Form\UserFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use MercurySeries\FlashyBundle\FlashyNotifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +18,28 @@ class AccountController extends AbstractController
     {
         return $this->render('account/show.html.twig', [
             'controller_name' => 'AccountController',
+        ]);
+    }
+
+    #[Route('/account/edit', name: 'app_account_edit')]
+
+    public function edit(Request $request, EntityManagerInterface $em, FlashyNotifier $flashy) : Response
+    {
+        $user = $this->getUser();
+        $form =  $this->createForm(GuideFormType::class, $user);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            $em->flush();
+            
+            $flashy->success('Modification réussie');
+
+            return $this->redirectToRoute('app_account');
+        }
+
+
+        return $this->render('account/edit.html.twig', [
+            'form'=> $form->createView()
         ]);
     }
 }
---
+++ src/Form/GuideFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Guide;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GuideFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class)
+            ->add('nom', TextType::class )
+            ->add('prenom' ,TextType::class)
+            ->add('parc' ,TextType::class)
+            ->add('sex' ,TextType::class)
+            ->add('age' ,TextType::class)
+           
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Guide::class,
+        ]);
+    }
+}
